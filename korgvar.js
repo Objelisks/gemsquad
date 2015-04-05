345103ed625a885c41683fcfb1b98f4ea83c9c08
Objelisks <supertyrian@gmail.com>
2015-04-04 22:46:30 -0700

    disable korgvars by default

--- korgvar.js
+++ korgvar.js
@@ -1,16 +1,21 @@
 define(function(require) {
-	var socket = io('ws://localhost:5000/');
+
+	var disable = true;
+
+	var socket = disable ? null : io('ws://localhost:5000/');
 	var vars = {};
 	for(var i=0; i<64; i++) {
 		vars[i] = 0.5;
 	}
 
-	socket.on('connect', function() {
-		socket.send('hi');
-		socket.on('message', function(message) {
-			vars[message.data[1]] = message.data[2] / 128.0;
+	if(!disable) {
+		socket.on('connect', function() {
+			socket.send('hi');
+			socket.on('message', function(message) {
+				vars[message.data[1]] = message.data[2] / 128.0;
+			});
 		});
-	});
+	}
 
 	return vars;
 });
